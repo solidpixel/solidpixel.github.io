63e35d08f8aa39e101e4fa275adb9393148d6b1b
Peter Harris <peter.harris@arm.com>
2024-07-31 08:54:07 +0100

    Clean up some visuals

--- javascript/toggle.js
+++ javascript/toggle.js
@@ -6,13 +6,28 @@ $('#blogsort').click(function() {
 
     $active = $(this).find('.active')[0];
     if ($active.id == 'sorttopic') {
-        console.log("Show by topic");
         $('#postdate').hide();
         $("#posttopic").show();
+
+        // Sort by topic is default, so just use default URL
+        var newURL = $(location).attr('href').split('?')[0];
     } else {
-        console.log("Show by date");
         $('#postdate').show();
         $("#posttopic").hide();
+
+        // Sort by date is extra behavior so set query parameter
+        var newURL = $(location).attr('href').split('?')[0] + '?sort=date';
     }
 
+    window.history.replaceState( {} , document.title, newURL);
+});
+
+
+$(document).ready(function() {
+    var string = $(location).attr('href');
+    var substring = '?sort=date';
+
+    if (string.indexOf(substring) !== -1) {
+        $('#blogsort').trigger('click');
+    }
 });
